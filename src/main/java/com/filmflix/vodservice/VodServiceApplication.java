47ac177b530cd9d136f3ff1420db6c8ed56fed3a
Michal <michal.olejko2@gmail.com>
2020-03-14 18:04:34 +0100

    initial commit

---
+++ src/main/java/com/filmflix/vodservice/VodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.filmflix.vodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VodServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filmflix/vodservice/VodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmflix.vodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
